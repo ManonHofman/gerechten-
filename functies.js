ba76962621c34f0e5f4474c68461b862acaa30cb
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-22 15:44:48 +0200

    Add files via upload

---
+++ functies.js
@@ -0,0 +1,22 @@
+const makePizza = function() {
+    console.log ("Maak het deeg")
+    console.log ("Voeg de tomatensaus toe")
+    console.log ("Voeg toppings toe naar smaak")
+    console.log ("Doe de pizza in de voorverwarmde oven voor 15 minuten")
+}
+
+// Using a function
+
+makePizza();
+makePizza();
+
+const makeSushi = function() {
+    console.log ("Kook de rijst")
+    console.log ("Snijd de groente en vis")
+    console.log ("Voeg toppings toe naar smaak")
+    console.log ("Rol de sushi's op en snijd ze in stukjes!")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
